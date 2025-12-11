38562f73d99c0695598d0958857752b2dce70275
Pavanakv <pavanakv557@gmail.com>
2025-12-11 17:58:44 +0530

    initial Commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL,
+  withCredentials: false 
 });
 
 export default API;
